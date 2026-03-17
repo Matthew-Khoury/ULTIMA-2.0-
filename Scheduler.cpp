9e0f05897f562b331d93f7fe9fadcf842ae47883
m-priestly <m.priestly@outlook.com>
2026-03-17 16:15:50 -0400

    Update Scheduler.cpp
    Updated Scheduler.cpp file

--- Scheduler.cpp
+++ Scheduler.cpp
@@ -16,14 +16,19 @@ struct tcb {
 };
 
 class scheduler{
-    int current_task; 
+    // the orginal line from Lab 12: int current_task; 
+    //the replacement lines to implement the linked list
+    tcb* current;
+    tcb* tail;
     long current_quantum;
     int next_available_task_id;
     tcb task_table[MAX_TASKS]; // this is an Array
 
 public:
 scheduler(){
-    current_task= -1;
+    // current_task= -1;
+    current = nullptr;
+    tail = nullptr;
     next_available_task_id =0;//start the system start at 0
     current_quantum=300; 
 }
@@ -44,12 +49,13 @@ string get_state(int the_taskid){
     return(task_table[the_taskid].state);
 }
 int get_task_id(){
-    return current_task;
+    return (current!= nullptr) ? current-> task_id: -1; // will return the current task
 }
+
 int create_task(){
     if(next_available_task_id<MAX_TASKS){
         cout<< "Creating task # " << next_available_task_id<<endl;
-        task_table[next_available_task_id].task.id = next_available_task_id << endl;
+        task_table[next_available_task_id].task_id = next_available_task_id << endl;
         task_table[next_available_task_id].state = READY;
         task_table[next_available_task_id].next = NULL; 
 
@@ -65,44 +71,50 @@ void start(){
     cout<< "................." << endl;
     cout<< ".................SCHEDULING STARTED" << endl;
     cout<< ".................\n" << endl;
-    task_table[0].start_time = clock();
-    task_table[0].state = RUNNING;
-    current_task = 0;
+    //task_table[0].start_time = clock();
+    //task_table[0].state = RUNNING;
+    current = tail->next; //the head node for the linked list 
+    current ->start_time = clock();
+    current-> state = RUNNING;
     set_quantum(1000/MAX_TASKS); //setting the fixed amount of time the CPU will allocate for the process
 
     sleep(1);
 
 }
 void yield (){
+
     int counter = 0;
-    cout << "Current Task # "<< current_task << " is trying to Yield" << endl;
+    cout << "Current Task # "<< current->task_id << " is trying to Yield" << endl;
     
-    clock_t elaspsed_time = clock() - task_table[current_task].start_time;
-    cout << "Task: " << current_task << ", Elapsed time: "<< elaspsed_time << endl;
+    clock_t elaspsed_time = clock() - current->start_time;
+    cout << "Task: " << current->task_id << ", Elapsed time: "<< elaspsed_time << endl;
 
     if(elaspsed_time >= current_quantum){
-        cout << "Yielding...(Switching from task #" << current_task << " to next ready task)" <<endl;
+        cout << "Yielding...(Switching from task #" << current->task_id << " to next ready task)" <<endl;
 
         //if running, we should change it to ready since the quantime itme has run out
-        if(task_table[current_task].state == RUNNING)
-            task_table[current_task].state = READY;
-        current_task = (current_task +1) % MAX_TASKS;
-        while(task_table[current_task].state != READY && counter < MAX_TASKS-1){ //we are subtracting one because we can not surpase the max tasks
-            current_task = (current_task + 1) % MAX_TASKS;
+        if(current->state == RUNNING)
+            current->state = READY;
+       // current_task = (current_task +1) % MAX_TASKS; from lab 12 replaced by the following line
+       current = current->next;
+
+        while(current->state != READY && counter < MAX_TASKS-1){ //we are subtracting one because we can not surpase the max tasks
+            //current_task = (current_task + 1) % MAX_TASKS;
+            current = current->next;
             counter ++;
 
         }
-        if (counter < MAX_TASKS -1 && task_table[current_task].state == READY){
-            task_table[current_task].start_time = clock();
-            task_table[current_task].state = RUNNING;
-            cout <<"Started Running task # "<<current_task <<endl;
+        if (counter < MAX_TASKS -1 && current->state == READY){
+            current->start_time = clock();
+            current->state = RUNNING;
+            cout <<"Started Running task # "<<current->task_id <<endl;
         }else{
         cout << "POSSIBLE DEAD LOCK"<< endl;
     }
     
     }
     else
-    cout<<"NO YIELD!  (task: " << current_task << " Still have some quantum left)" << endl;
+    cout<<"NO YIELD!  (task: " << current->task_id << " Still have some quantum left)" << endl;
 }
 
 void dump(){
@@ -110,11 +122,11 @@ void dump(){
     cout <<"Quantum = " << current_quantum<< endl;
 
     cout << "Task ID\t Elapsed Time\tState"<<endl;
-    for(int i = 0; i<MAX_TASKS; i++){
+    for(int i = 0; i<next_available_task_id; i++){
         clock_t elapsed_time = clock() - task_table[i].start_time;
         printf("%6d\t%8d\t%s",task_table[i].task_id, elapsed_time, task_table[i].state.c_str() );
 
-        if(i == current_task)
+        if(i == current->task_id)// ** ask the group to 
             cout <<" <---CURRENT PROCESS";
         cout << endl; 
     }
