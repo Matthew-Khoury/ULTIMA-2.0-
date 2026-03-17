5fb8d181bc4b4148fafe57fc7ffa1fdfc9081430
m-priestly <m.priestly@outlook.com>
2026-03-17 12:07:43 -0400

    Create Scheduler.cpp
    Framework for Scheduler Code

---
+++ Scheduler.cpp
@@ -0,0 +1,143 @@
+#include "Queue.h"
+
+const string READY = "Ready";
+const string RUNNING = "Running";
+const string BLOCKED = "Blocked"; 
+const string DEAD = "Dead";
+
+const int MAX_TASKS = 3; //how do we determine the number of tasks allowed?
+
+struct tcb {
+    int task_id;
+    string state;
+    clock_t start_time;
+    tcb *next; //this line creates a pointer to the next node in the linked list
+
+};
+
+class scheduler{
+    int current_task; 
+    long current_quantum;
+    int next_available_task_id;
+    tcb task_table[MAX_TASKS]; // this is an Array
+
+public:
+scheduler(){
+    current_task= -1;
+    next_available_task_id =0;//start the system start at 0
+    current_quantum=300; 
+}
+~scheduler(){
+    cout<<"Existing Scheduler...."<<endl;
+}
+
+void set_quantum(long quantum){
+    current_quantum = quantum;
+}
+long get_quantum(){
+    return(current_quantum);
+}
+void set_state(int the_taskid, string the_state){
+    task_table[the_taskid].state = the_state;
+}
+string get_state(int the_taskid){
+    return(task_table[the_taskid].state);
+}
+int get_task_id(){
+    return current_task;
+}
+int create_task(){
+    if(next_available_task_id<MAX_TASKS){
+        cout<< "Creating task # " << next_available_task_id<<endl;
+        task_table[next_available_task_id].task.id = next_available_task_id << endl;
+        task_table[next_available_task_id].state = READY;
+        task_table[next_available_task_id].next = NULL; 
+
+        next_available_task_id++;
+        return(next_available_task_id -1);
+    }
+    else{
+        cout << "Create_task() FAILED: Available tasks exceeded. MAX_TASKS = " << MAX_TASKS << endl;
+        return(-1); //return error
+    }
+}
+void start(){
+    cout<< "................." << endl;
+    cout<< ".................SCHEDULING STARTED" << endl;
+    cout<< ".................\n" << endl;
+    task_table[0].start_time = clock();
+    task_table[0].state = RUNNING;
+    current_task = 0;
+    set_quantum(1000/MAX_TASKS); //setting the fixed amount of time the CPU will allocate for the process
+
+    sleep(1);
+
+}
+void yield (){
+    int counter = 0;
+    cout << "Current Task # "<< current_task << " is trying to Yield" << endl;
+    
+    clock_t elaspsed_time = clock() - task_table[current_task].start_time;
+    cout << "Task: " << current_task << ", Elapsed time: "<< elaspsed_time << endl;
+
+    if(elaspsed_time >= current_quantum){
+        cout << "Yielding...(Switching from task #" << current_task << " to next ready task)" <<endl;
+
+        //if running, we should change it to ready since the quantime itme has run out
+        if(task_table[current_task].state == RUNNING)
+            task_table[current_task].state = READY;
+        current_task = (current_task +1) % MAX_TASKS;
+        while(task_table[current_task].state != READY && counter < MAX_TASKS-1){ //we are subtracting one because we can not surpase the max tasks
+            current_task = (current_task + 1) % MAX_TASKS;
+            counter ++;
+
+        }
+        if (counter < MAX_TASKS -1 && task_table[current_task].state == READY){
+            task_table[current_task].start_time = clock();
+            task_table[current_task].state = RUNNING;
+            cout <<"Started Running task # "<<current_task <<endl;
+        }else{
+        cout << "POSSIBLE DEAD LOCK"<< endl;
+    }
+    
+    }
+    else
+    cout<<"NO YIELD!  (task: " << current_task << " Still have some quantum left)" << endl;
+}
+
+void dump(){
+    cout << "------------------------------PROCESS TABLE------------------------------------"<<endl;
+    cout <<"Quantum = " << current_quantum<< endl;
+
+    cout << "Task ID\t Elapsed Time\tState"<<endl;
+    for(int i = 0; i<MAX_TASKS; i++){
+        clock_t elapsed_time = clock() - task_table[i].start_time;
+        printf("%6d\t%8d\t%s",task_table[i].task_id, elapsed_time, task_table[i].state.c_str() );
+
+        if(i == current_task)
+            cout <<" <---CURRENT PROCESS";
+        cout << endl; 
+    }
+    cout<<"----------------------------------------------------------------------------------\n"<<endl;
+}
+
+
+};
+
+int main(){
+    scheduler swapper;
+    int t_id;
+
+    for(int i = 0; i< 4; i++)
+    t_id = swapper.create_task();
+
+    swapper.dump();
+    swapper.start();
+    swapper.dump();
+
+    for(int i = 0; i<3; i++){
+        waste_time(3);
+        swapper.yield();
+        swapper.dump();
+    }
+}
